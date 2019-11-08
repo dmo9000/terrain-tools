d68f31c6d1bceeb805ce9871ad1e34ad79560240
Dan Morriss <daniel_morriss@yahoo.com.au>
2019-11-09 10:37:09 +1100

    WIP.

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-g -ggdb
+
+all: ss
+
+
+clean: 
+	rm -f ss ss.exe
--- ss.c
+++ ss.c
@@ -1,1 +1,23 @@
 /* spatial subdivision */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define MIDPOINT	127
+
+uint8_t landscape[128][128];
+
+int main(int argc, char *argv[])
+{
+	int rows = 1, cols = 1;
+	int i = 0, j = 0;
+
+	/* initialize entire array to midpoint */
+
+	for (i = 0; i < 128; i++) {
+		for (j = 0; j < 128 ; j++) {
+			landscape[i][j] = MIDPOINT;
+			}
+		}
+
+}
